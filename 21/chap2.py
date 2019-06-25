8a27fa91011347f54f419fdfa1d0d3e9bd8a7900
Henry Lei <henrylei@Henrys-iMac.local>
2019-06-24 23:11:31 -0700

    edited and revised

--- 21/chap2.py
+++ 21/chap2.py
@@ -22,3 +22,27 @@ True = 2
 x = False
 if(x):
     print(x)
+print(1)
+x = 2
+print(x)
+print(1 + 1)
+cookies = 15
+print(cookies/4)
+print(cookies//4)
+print(99.0/100)
+print(15.0/4)
+fruit = "banana"
+baked_good = " nut bread"
+print(fruit + baked_good)
+print('fun' * 3)
+#name = input("Please enter your name: ")
+#print(name)
+#age = input("please e#nter your age: ")
+#print(age)
+print(17 + 3)
+#compute the percentage of the hour that has elapsed
+#percentage = (minute * 100) // 60 #caution: integer division
+for name in ["Alice", "Bjorn", "Cayman", "Duanphen", "esfir", "farah"]:
+    print("I know " + name + ".")
+    print("%s is a friend of mine." % (name))
+print("Those are the people I know.")
