d298e20846c34f6c1108185832689edf2352b7c8
Henry Lei <henrylei@Henrys-iMac.local>
2019-06-24 22:17:33 -0700

    added chap 2

--- 21/chap1.py
+++ 21/chap1.py
@@ -3,3 +3,5 @@ print('this is a test')
 for i in [1, 2, 4]:
     i = i + 3
     print(i)
+print("My first program adds two numbers, 2 and 3:")
+print(2 + 3)
---
+++ 21/chap2.py
@@ -0,0 +1,24 @@
+print(4)
+print("hello, world!")
+print(type("hello, world!"))
+print(type(17))
+print("tom's friend")
+print('tom\'s friend')
+print(1,000,000)
+message = "What's up, Doc?"
+n = 17
+pi = 3.14159
+print(type(message))
+print(type(n))
+print(type(pi))
+print(message)
+print(n)
+print(pi)
+classes = 5
+true = 3
+True = 2
+#print(True)
+#False = 4
+x = False
+if(x):
+    print(x)
