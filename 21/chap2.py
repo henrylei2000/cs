375a24bdfd5275015f194d8d7e15bf4345af0ab2
Henry Lei <henrylei@Henrys-iMac.local>
2019-06-27 21:31:40 -0700

    new edits

--- 21/chap2.py
+++ 21/chap2.py
@@ -22,7 +22,6 @@ print('tom\'s friend')
 # number format
 print(1,000,000)
 
-
 # test True and False keywords
 classes = 5
 true = 3
@@ -44,23 +43,58 @@ baked_good = " nut bread"
 print(fruit + baked_good)
 print('fun' * 3)
 
-name = input("Please enter your name: ")
-print("Hi " + name + ", nice to meet you!")
-
-age = input("please enter your age: ")
-if int(age) >= 18:
-    print("You're such a great young person!")
-else:
-    print("You're too young to play the game!")
-
-print(age)
-
-
+# input doesn't work with Atom IDE
+# name = input("Please enter your name: ")
+# print("Hi " + name + ", nice to meet you!")
+# age = input("please enter your age: ")
+# if int(age) >= 18:
+#     print("You're such a great young person!")
+# else:
+#     print("You're too young to play the game!")
+#
+# print(age)
 
 print(17 + 3)
 #compute the percentage of the hour that has elapsed
 #percentage = (minute * 100) // 60 #caution: integer division
+
+# for loop
 for name in ["Alice", "Bjorn", "Cayman", "Duanphen", "esfir", "farah"]:
     print("I know " + name + ".")
     print("%s is a friend of mine." % (name))
 print("Those are the people I know.")
+
+# the range function
+# script mode - no evaluation & print
+range(0, 10)
+
+import sys
+print(sys.version_info)
+print(type(range(0, 10)))
+print(list(range(0, 10))) #convert to a list
+print(list(range(2, 3))) #convert to a list, includes 2 but not 3
+for number in range(0, 10):
+    print(number)
+print(list(range(5)))
+print(list(range(0,20,2)))
+
+# range and int
+total_str = input("How many numbers should I print? ")
+total = int(total_str)
+for number in range(0, total):
+    print(number)
+
+start = int(input("Which number should I print first? "))
+stop = int(input("Which number should make the loop stop? "))
+for number in range(start,stop):
+    print(number)
+
+# Accumulators
+last_number_str = input("Sum up to what number? ")
+last_number = int(last_number_str)
+sum = 0 # this is our accumalator
+for number in list(range(0, last_number+1)):
+    sum = sum + number
+print("The sum of those numbers is: %d" % (sum)) # +1 to include last_number itself
+
+# REPL mode; command line mode; Python Prompt + Evalation and print
