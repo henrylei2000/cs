7ef068919a78164156ad4b0451afe76418082280
Jin Lei <jin.lei@ubc.ca>
2020-05-05 01:21:33 -0700

    kmt

--- pong/kmt.py
+++ pong/kmt.py
@@ -7,6 +7,7 @@ from kivy.animation import Animation
 import time
 import threading
 from random import random as r
+from math import *
 
 class results(Screen):
     reset = NumericProperty(0)
@@ -22,28 +23,84 @@ class results(Screen):
         t.start()
 
     def draw(self):
-        p1 = [100, 100]
-        p2 = [200, 400]
-        p3 = [400, 400]
-        p4 = [300, 100]
-        points = [p1, p2, p4]
 
-        with self.canvas:
-            Color(r(), 1, 1, mode='hsv')
-            Line(points=p1 + p2 + p4 + p1, width=2)
+        c = (960, 540)  # center
+        half_w = 40
 
-        self.patternize(points, 0.1)
+        ngon = 12
+        # sun = self.draw_ngon(ngon, 290, c)
+        triangles = self.draw_triangle(12, 250, c)
+        for i in range(len(triangles)):
+            self.patternize(triangles[i], 0.1)
 
-        with self.canvas:
-            self.rot = Rotate()
-            self.rot.axis = (0, 0, 1)
-            self.rot.origin = self.center
-            self.rot.angle = 10
-            Line(ellipse=(400, 440, 200, 100, 0, 360), width=2)
-            Line(ellipse=(400, 440, 190, 90, 0, 360), width=2)
+        # self.patternize(sun, 0.5)
+
+    def draw_ngon(self, n, radius, position):
+        pi2 = 2 * 3.14
+        points = []
+        for i in range(n):
+            points.append([cos(i / n * pi2) * radius + position[0], sin(i / n * pi2) * radius + position[1]])
+
+        return points
+
+    def draw_triangle(self, n, radius, position):
+        pi2 = 2 * 3.14
+        points = []
+        inner_triangles = []
+        for i in range(n):
+            points.append([cos((i + .5)/ n * pi2) * radius + position[0], sin((i + .5) / n * pi2) * radius + position[1]])
+
+        for i in range(n):
+            if i < n - 1:
+                inner_triangles.append([points[i], points[i + 1], position])
+            else:
+                inner_triangles.append([points[i], points[0], position])
+
+        r = radius + 250
+        triangles = []
+        for i in range(n):
+            top = [cos((i + 1) / n * pi2) * r + position[0], sin((i+1) / n * pi2) * r + position[1]]
+            if i < n - 1:
+                triangles.append([points[i], points[i+1], top])
+            else:
+                triangles.append([points[i], points[0], top])
+
+        all_points = []
+        for i in range(n):
+            all_points.append(inner_triangles[i])
+            all_points.append(triangles[i])
+
+        return all_points
+
+    def mirror(self, points, center, direction="h"):
+        mirror_side = []
+        for p in points:
+            if direction == "h":  # horizontal flip
+                mp = self.mirror_x(p, center)
+            else:
+                mp = self.mirror_y(p, center)
+            mirror_side.append(mp)
+
+        mirror_side.reverse()
+        return mirror_side
+
+    def mirror_x(self, point, center):
+        mirror = [point[0], point[1]]
+        mirror[0] += (center - point[0]) * 2  # gap
+        return mirror
+
+    def mirror_y(self, point, center):
+        mirror = [point[0], point[1]]
+        mirror[1] -= (point[1] - center) * 2  # gap
+        return mirror
 
     def patternize(self, points, pace):
-        for i in range(10):
+
+        with self.canvas:
+            Color(.1, .1, .9)
+            Line(points=(points+[points[0]]), width=2)
+
+        for i in range(50):
             # [[x, y], [x, y], [x, y], [x, y]]
 
             if self.too_close(points):
@@ -72,9 +129,9 @@ class results(Screen):
                 self.ss.clear()
                 self.anim_pt = collector[j]
                 self.ss = collector[j]
-                anim = Animation(anim_pt=collector[j + 1], d=1)
+                anim = Animation(anim_pt=collector[j + 1], d=.2)
                 anim.start(self)
-                time.sleep(1)
+                time.sleep(.3)
                 anim.stop(self)
 
         self.ss.clear()
@@ -96,8 +153,8 @@ class results(Screen):
 
         # draw the updated line
         with self.canvas:
-            Color(r(), 1, 1, mode='hsv')
-            self.line = Line(points=points, width=2)
+            Color(.9, .9, .9)
+            self.line = Line(points=points, width=1)
 
 
 class mazeupdateApp(App):
